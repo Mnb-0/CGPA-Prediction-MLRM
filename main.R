61e7cff719c0bb1f238dd5062cd1aa99517b1639
Muneeb <150353438+Mnb-0@users.noreply.github.com>
2024-11-18 23:59:51 +0500

    Renamed columns
    Added meaningful names to columns instead of the questions asked in the form.

--- main.R
+++ main.R
@@ -5,6 +5,12 @@ library(dplyr)
 # Load the dataset
 data <- read.csv("form_responses.csv")
 
+
+# Rename the columns
+colnames(data) <- c("Time","Name","RollNumber","CGPA","Avg Attendance", "Highschool %", "Study Hours/ Week","Exercise Hours/ Week","Sleep Hours/ Day")
+# Confirm the updated column names
+colnames(data)
+
 # Confirm the changes
 str(data)
 head(data)
