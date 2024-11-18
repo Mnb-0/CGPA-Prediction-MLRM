f4ba02305d0366df369dec974f9b42db1d65eb51
Muneeb <150353438+Mnb-0@users.noreply.github.com>
2024-11-19 00:04:50 +0500

    Added new features
    Added a column for batch and debarment status.

--- main.R
+++ main.R
@@ -11,6 +11,40 @@ colnames(data) <- c("Time","Name","RollNumber","CGPA","Avg Attendance", "Highsch
 # Confirm the updated column names
 colnames(data)
 
+# Drop the first two columns
+data <- data[, -c(1, 2)]
+
+# Add a new column for Batch based on the first two digits of the RollNumber
+data$Batch <- substr(data$RollNumber, 1, 2)
+data$Batch <- as.numeric(data$Batch)
+
+#Drop RollNumber
+data <- data[, -1]
+
+# Add a new column for MeanAttendance based on the average of the intervals in Avg Attendance
+data$MeanAttendance <- sapply(data$`Avg Attendance`, function(interval) {
+  # Check if the interval is NA or empty
+  if (is.na(interval) || interval == "") {
+    return(NA)  # Assign NA for missing or empty values
+  }
+  
+  # Remove the '%' character and split the interval by '-'
+  interval_cleaned <- gsub("%", "", interval)
+  bounds <- as.numeric(unlist(strsplit(interval_cleaned, "-")))
+  
+  # calculate the mean
+  if (length(bounds) == 2) {
+    return(mean(bounds))
+  } else if (length(bounds) == 1) {
+    return(bounds)  # Handle single numeric values
+  } else {
+    return(NA)  # Assign NA for unexpected formats
+  }
+})
+
+# Add a column for debarrment status
+data$Debarred <- ifelse(data$MeanAttendance < 80, "Yes", "No")
+
 # Confirm the changes
 str(data)
 head(data)
